fc3ae9d90a312cb7be1dee78224dc3bd256e5fe7
MohitJindal1991 <90623594+MohitJindal1991@users.noreply.github.com>
2025-02-25 22:50:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+const Title=document.querySelector("#title");
+const Author=document.querySelector("#author");
+const Isbn=document.querySelector("#isbn");
+const submitButton=document.querySelector("#button");
+
+const tableData=document.querySelector(".table");
+submitButton.addEventListener('click',(e)=>{
+	e.preventDefault();
+
+	const newRow=document.createElement('tr');
+	newRow.innerHTML=`<tr>
+<td>${Title.value}</td>
+<td>${Author.value}</td>
+<td>${Isbn.value}</td>
+<td class="delete">❌</td>
+</tr>`
+	tableData.append(newRow)
+	Title.value="";
+	Author.value="";
+	Isbn.value="";
+});
+
+tableData.addEventListener('click',(e)=>{
+	if(e.target.classList.contains("delete")){
+	e.target.parentNode.parentNode.remove();
+	}
+})
